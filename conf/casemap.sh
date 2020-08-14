985befb60200d572f229368b60c9d7f3dc5b1cb2
Alexandru Negrila <alex-codes@arntech.ro>
2020-08-14 11:16:48 +0000

    Casemap update - test

--- conf/casemap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cat <<EOF
-	casemap = "${ANOPE_CASEMAP:-rfc1459}"
-EOF
--- conf/config.sh
+++ conf/config.sh
@@ -12,4 +12,9 @@ define
     name = "services.name"
     value = "${ANOPE_SERVICES_NAME:-services.localhost.net}"
 }
+define
+{
+    name = "case.map"
+    value = "${ANOPE_CASEMAP:-rfc1459}"
+}
 EOF
